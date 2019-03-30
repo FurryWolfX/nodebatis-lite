437df47cce890d95c87eb0355cbc5f61b41dd308
wolfx <digimonx@qq.com>
2019-03-30 12:16:27 +0800

    1.0.7 完善d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,42 @@
+import { IPromise } from "promise";
+import { Pool } from "mysql";
+
+interface PoolConfig {
+  minSize: number;
+  maxSize: number;
+  connectionLimit: number;
+}
+
+interface NodeBatisLiteConfig {
+  debug?: boolean;
+  debugCallback?: Function;
+  dialect: string;
+  host: string;
+  port: number;
+  database: string;
+  user: string;
+  password: string;
+  camelCase?: boolean;
+  pool?: PoolConfig
+}
+
+export default class NodeBatisLite {
+  constructor(dir: string, config: NodeBatisLiteConfig);
+
+  execute(sql: string, params: Array<any>): IPromise;
+
+  query(key: string, data: Object): IPromise;
+
+  insert(tableName: string, data: Object): IPromise;
+
+  update(tableName: string, data: Object, idKey?: string): IPromise;
+
+  delete(tableName: string, id: string | number, idKey?: string): IPromise;
+
+  find(tableName: string, dataArray: Array<string>, id: string, paramKey: string): IPromise;
+
+  getPool(): Pool
+}
+
+
+
