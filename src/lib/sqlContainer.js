fbe4de5f0f2a4a84d2a03614607ec6af641e38f5
wolfx <digimonx@qq.com>
2019-04-23 23:58:25 +0800

    merge from zjp1029/nodebatis: fix bug of behind where has space and no params,has not ignore where

--- src/lib/sqlContainer.js
+++ src/lib/sqlContainer.js
@@ -97,7 +97,7 @@ class SqlContainer {
       }
     }
     result = rawSql.join(" ");
-    const lastWhereReg = /\s+where$/i;
+    const lastWhereReg = /\s+where\s*$/i;
     const whereAndReg = /\s+where\s+and\s+/gi;
     const whereOtherReg = /\s+where\s+(union\s+|order\s+|group\s+|limit\s+)/gi;
     const searchReg = /\s+where\s+/i;
